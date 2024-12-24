8de8ccb9c3486fe78004e454578784916b9d279c
md-shahid-ansari <sajarsadaf2000@gmail.com>
2024-12-24 14:02:51 +0530

    ready to deploy.

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline
 from flask_cors import CORS
+from flask import send_from_directory
+import os
+from dotenv import load_dotenv
 
-app = Flask(__name__)
-CORS(app)  # Enable CORS
+# Load environment variables
+load_dotenv()
+
+frontend = os.getenv('FRONTEND', 'http://localhost:3000')
+
+app = Flask(__name__, static_url_path='', static_folder='./build', template_folder='./build')
+CORS(app, resources={r"*": {"origins": frontend}})
 
 # Load the NLP model
 nlp = pipeline("question-answering")
@@ -23,6 +31,22 @@ def query():
 
     except Exception as e:
         return jsonify({'error': str(e)})
+    
+
+# Route to serve the React app's main index.html
+@app.route('/')
+@app.route('/<path:path>')
+def serve_react(path=''):
+    if path and os.path.exists(os.path.join(app.static_folder, path)):
+        return send_from_directory(app.static_folder, path)
+    else:
+        return send_from_directory(app.static_folder, 'index.html')
+
+# Additional route to serve static assets (CSS, JS, etc.)
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory(os.path.join(app.static_folder, 'static'), path)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
